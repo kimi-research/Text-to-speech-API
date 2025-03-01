c7d166ddb0d634716bee29bf392601df7b1aa1f8
Kimihiro Hattori <hattori.kimihiro@g.sp.m.is.nagoya-u.ac.jp>
2025-03-01 12:24:05 +0900

    update: run.sh for deploy

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-
+# activate virtual env
+source /home/k_hattori/Text-to-speech-API/venv/bin/activate
+# set LOGFILE
+LOGFILE="startat_$(date '+%Y%m%d_%H%M%S').log"
 # Start API (fastAPI)
-uvicorn app.main:app --reload
+nohup uvicorn app.main:app --host=0.0.0.0 --port=80 &> "$LOGFILE" &
